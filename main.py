8634b1c93499498ce53c299a135d7dc1d5e0549c
Ali Slaoui <alislaoui701@gmail.com>
2025-01-31 17:24:38 +0100

    requirements

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ st.set_page_config(
 
 hide_streamlit_style = """
     <style>
+    #appCreatorAvatar {visibility: hidden;}
     ._profileImage_gzau3_78 _lightThemeShadow_gzau3_95{display: none;}
     #MainMenu {visibility: hidden;}
     footer {display: none;}
